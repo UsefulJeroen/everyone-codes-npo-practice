56fcc992c2b2cbe269edb32ecea3ea72f95347bf
Jeroen <jeroen.b.k@hotmail.com>
2023-04-28 16:49:05 +0200

    clitool

---
+++ CliSearch/Program.cs
@@ -0,0 +1,37 @@
+﻿using CsvExtracter;
+using System.CommandLine;
+
+
+var nameOption = new Option<string>("--name",
+    description: "Name of the result")
+{
+    IsRequired = true
+};
+
+
+var rootCommand = new RootCommand("Search for a result");
+rootCommand.AddOption(nameOption);
+
+
+rootCommand.SetHandler(async (name) =>
+{
+    var cameraData = await CsvDataExtractor.ExtractCameraData();
+    //Todo?: add fuzzy search?
+    var camerasFound = cameraData.FindAll(x => x.Name.Contains(name));
+
+    if (!camerasFound.Any())
+    {
+        Console.WriteLine("Camera not found");
+    }
+    else
+    {
+        Console.WriteLine($"Camera('s) found: ");
+        foreach (var camera in camerasFound)
+        {
+            Console.WriteLine($"{camera.Id} | {camera.Name} | {camera.Latitude} | {camera.Longtitude}");
+        }
+    }
+}, nameOption);
+
+
+await rootCommand.InvokeAsync(args);
